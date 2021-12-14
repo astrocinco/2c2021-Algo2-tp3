c1d160b654ac1b05bb68f80e6de0ee2836cca700
astrocinco <santiagolps3@gmail.com>
2021-12-14 16:13:49 -0300

    .

---
+++ grafo.py
@@ -0,0 +1,39 @@
+class _Arista:
+    def __init__(self):
+        self.atributo1 = None # Poner nombre correcto. Esto es un ejemplo
+
+
+class Grafo:
+    def __init__(self):
+        self.atributo1 = None # Poner nombre correcto. Esto es un ejemplo
+        self.atributo2 = None # Poner nombre correcto. Esto es un ejemplo
+
+    def __str__(self): # Revisar si esto es así
+        pass
+
+    def agregar_vertice(self, vertice):
+        pass
+
+    def borrar_vertice(self, vertice):
+        pass
+
+    def agregar_arista(self, vertice_1, vertice_2, peso = 1):
+        pass 
+
+    def borrar_arista(self, vertice_1, vertice_2):
+        pass
+
+    def estan_unidos(self, vertice_1, vertice_2):
+        pass
+
+    def peso_arista(self, vertice_1, vertice_2):
+        pass
+
+    def obtener_vertices(self):
+        pass
+
+    def vertice_aleatorio(self):
+        pass
+
+    def aydacentes(self, vertice):
+        pass
