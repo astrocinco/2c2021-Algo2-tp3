53688cf9396d6e55885a0787a058715fa808480b
astrocinco <santiagolps3@gmail.com>
2021-12-23 18:49:20 -0300

    .

--- netstats.py
+++ netstats.py
@@ -52,7 +52,7 @@ def ciclo():
                 raise IndexError ("Número de variables incorrecto en 'rango'")
             tp3.todos_en_rango(grafo_netstats, param_ran[0], int(param_ran[1]))
 
-        elif input_terminal[0] == "diametro":
+        elif input_terminal[0] == "diametro": # -- HACER --
             tp3.diametro(grafo_netstats)
 
         elif input_terminal[0] == "lectura":
