e32177bd557975281f655332131c3625175885be
astrocinco <santiagolps3@gmail.com>
2021-12-24 11:51:24 -0300

    .

--- netstats.py
+++ netstats.py
@@ -1,8 +1,4 @@
-"""
-https://algoritmos-rw.github.io/algo2/tps/2021_2/tp3/
-Wiki ya parseada: https://drive.google.com/file/d/1JOxK7E0bqW3yfuj3niGpPOaWCX7vo_8Q/view
-Wiki ya parseada - REDUCIDO: https://drive.google.com/file/d/1b0fZPVE2e1z4TGFL9n4ZiqAnEMAU25rs/view
-"""
+#!/usr/bin/python3
 
 from grafo import Grafo
 import tp3
